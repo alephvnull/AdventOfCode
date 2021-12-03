def969f45a3163f21ac51b04e21deb61de24cdfb
genkuroo <genkurouy@gmail.com>
2021-12-03 19:08:03 +0100

    2021 / day 1

--- 2021/day01.jl
+++ 2021/day01.jl
@@ -1,5 +1,5 @@
-function parse_file(file_path :: String) :: Array{Int64, 1}
-    ln :: Array{Int64, 1} = Int64[]
+function parse_file(file_path) 
+    ln = Int64[]
     open(file_path) do file
         for line in eachline(file)
             push!(ln, parse(Int, line))
@@ -8,16 +8,16 @@ function parse_file(file_path :: String) :: Array{Int64, 1}
     return ln
 end
 
-function increases(ln :: Array{Int64, 1}) :: Int64
+function increases(ln)
     return [ 1 for i in 2:length(ln) if ln[i] > ln[i-1]] |> length
 end
 
-function threesumincreases(ln :: Array{Int64, 1}) :: Int64
+function threesumincreases(ln)
     return [ 1 for i in 4:length(ln) if ln[i] > ln[i-3]] |> length
 end
 
-@time part1 = "2021/input/input01.txt" |> parse_file |> increases
-@time part2 = "2021/input/input01.txt" |> parse_file |> threesumincreases 
+@time part1 = "data/2021/input01.txt" |> parse_file |> increases
+@time part2 = "data/2021/input01.txt" |> parse_file |> threesumincreases 
 
 println("Part 1 : $(part1)")
 println("Part 2 : $(part2)")
