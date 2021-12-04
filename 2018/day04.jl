2669d3d19d3e72bd3e30b3232b890132fcb21558
genkuroo <genkurouy@gmail.com>
2021-12-04 15:02:53 +0100

    2018 / day 4

--- 2018/day04.jl
+++ 2018/day04.jl
@@ -1,28 +1,14 @@
-function parse_file(file_path :: String) :: Array{String, 1}
-    ln :: Array{String, 1} = String[]
-    open(file_path) do file
-        for line in eachline(file)
-            push!(ln, line)
-        end
-    end
-    return ln
-end
-
-function records(arr :: Array{String, 1}) :: Dict{Int, Array{Int64, 1}}
-    lines :: Array{String, 1} = sort(arr)
-    sleep :: Dict{Int64, Array{Int64, 1}} = Dict()
-    guard :: Int64 = -1
-
-    pminute = x :: String -> parse(Int, x[16:17]) :: Int64
-
+function records(arr)
+    lines = sort(arr)
+    sleep = Dict()
+    guard = -1
+    pminute = x -> parse(Int, x[16:17]) 
     for line in lines
         if occursin("Guard", line)
             start = findfirst(isequal('#'), line)
             stop  = findnext(isequal(' '), line, start)
             guard = parse(Int, line[start+1:stop-1])
-
             (guard in keys(sleep)) ? nothing : sleep[guard] = zeros(Int, 60)
-
         elseif occursin("falls", line)
             start = pminute(line) + 1
             sleep[guard][start:end] .+= 1
@@ -35,30 +21,25 @@ function records(arr :: Array{String, 1}) :: Dict{Int, Array{Int64, 1}}
     return sleep
 end
 
-function sleepiest_guard(records :: Dict{Int, Array{Int64, 1}}) :: Int64
-
-    sguard :: Int64 = -1
-    asleep :: Int64 = -1
-
-    for (guard :: Int64, sleep :: Array{Int64, 1}) in records
+function sleepiest_guard(records)
+    sguard = -1
+    asleep = -1
+    for (guard, sleep) in records
         ∑ = sum(sleep)
-
         if ∑ > asleep
             sguard = guard
             asleep = ∑
         end
     end
-
-    mmin :: Int64 = argmax(records[sguard]) - 1
+    mmin = argmax(records[sguard]) - 1
     return sguard * mmin
 end
 
-function most_frequently(records :: Dict{Int, Array{Int64, 1}}) :: Int64
-    sguard :: Int64 = -1
-    asleep :: Int64 = -1
-    mmin   :: Int64 = -1
-
-    for (guard :: Int64, sleep :: Array{Int64, 1}) in records
+function most_frequently(records)
+    sguard  = -1
+    asleep  = -1
+    mmin    = -1
+    for (guard, sleep) in records
         Θₘₐₓ = maximum(sleep)
         if Θₘₐₓ > asleep
             sguard = guard
@@ -69,8 +50,8 @@ function most_frequently(records :: Dict{Int, Array{Int64, 1}}) :: Int64
     return sguard * mmin 
 end
 
-@time part1 = "2018/input/input04.txt" |> parse_file |> records |> sleepiest_guard
-@time part2 = "2018/input/input04.txt" |> parse_file |> records |> most_frequently
+@time part1 = "data/2018/input04.txt" |> readlines |> records |> sleepiest_guard
+@time part2 = "data/2018/input04.txt" |> readlines |> records |> most_frequently
 
 println("Part 1 : $(part1)")
 println("Part 2 : $(part2)")
