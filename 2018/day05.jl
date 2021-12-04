d0ceb072b8ccdceb1213371f775cdab21f652ab1
genkuroo <genkurouy@gmail.com>
2021-12-04 15:03:08 +0100

    2018 / day 5

--- 2018/day05.jl
+++ 2018/day05.jl
@@ -1,7 +1,7 @@
-polymer = file_path :: String -> convert.(Char, read(open(file_path, "r"))) :: Array{Char, 1}
+polymer = file_path -> convert.(Char, read(open(file_path, "r"))) 
 
-function react(polymer :: Array{Char,1}) :: Array{Char,1}
-    chars :: Array{Char,1} = Array([])
+function react(polymer) 
+    chars = Array([])
     for char in polymer
         !(isempty(chars)) && (lowercase(char) == lowercase(chars[end])) && (chars[end] != char) ? pop!(chars) : push!(chars, char)
     end
@@ -9,8 +9,8 @@ function react(polymer :: Array{Char,1}) :: Array{Char,1}
 end
 
 
-function full_react(polymer :: Array{Char,1}) :: Int64
-    mln :: Int64 = typemax(Int64)
+function full_react(polymer)
+    mln = typemax(Int64)
     for char in 'a':'z'
         len = filter(c -> lowercase(c) ≠ char, polymer) |> react |> join |> length 
         mln > len ? mln = len : nothing
@@ -18,8 +18,8 @@ function full_react(polymer :: Array{Char,1}) :: Int64
     return mln
 end
 
-@time part1 = "2018/input/input05.txt" |> polymer |> react |> join |> length 
-@time part2 = "2018/input/input05.txt" |> polymer |> react |> full_react
+@time part1 = "data/2018/input05.txt" |> polymer |> react |> join |> length 
+@time part2 = "data/2018/input05.txt" |> polymer |> react |> full_react
 
 println("Part 1 : $(part1)")
 println("Part 2 : $(part2)")
