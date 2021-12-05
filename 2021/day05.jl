57c68c290a591deb1372e3d37a23ff21deec4afc
genkuroo <genkurouy@gmail.com>
2021-12-05 10:29:56 +0100

    2021 / day 5

--- 2021/day05.jl
+++ 2021/day05.jl
@@ -1,22 +1,19 @@
-input = readlines("data/2021/input05.txt")
-
-zz = zeros(Int, 1000, 1000)
-
 hvline(x1, y1, x2, y2) = (x1 == x2) ||  (y1 == y2)
+minmax(a,b) = (min(a,b), max(a,b))
+fxy(line) = split(line, " -> ") .|> x -> parse.(Int, split(x, ","))
 
-for ll in input 
-    x1y1, x2y2 = split(ll, " -> ")
-    x1, y1 = parse.(Int, split(x1y1, ","))
-    x2, y2 = parse.(Int, split(x2y2, ","))
-    x1, x2 = min(x1,x2), max(x1,x2)
-    y1, y2 = min(y1,y2), max(y1,y2)
-
-    if hvline(x1,y1,x2,y2)
-        zz[x1+1:x2+1, y1+1:y2+1] .+= 1
+function only_hv(input)
+    zz = zeros(Int, 1000, 1000)
+    for line ∈ input
+        (x1,y1),(x2,y2) = fxy(line)
+        if hvline(x1,y1,x2,y2)
+            x1, x2 = minmax(x1,x2)
+            y1, y2 = minmax(y1,y2)
+            zz[x1+1:x2+1, y1+1:y2+1] .+= 1
+        end       
     end
-
+    count(zz .> 1)
 end
-println(count(zz .> 1))
 
 function diagonal(x1,y1,x2,y2)
     xinc = x2 > x1 ? 1 : -1
@@ -24,22 +21,26 @@ function diagonal(x1,y1,x2,y2)
     return x1:xinc:x2, y1:yinc:y2
 end
 
-zz = zeros(Int, 1000, 1000)
-
-for ll in input 
-    x1y1, x2y2 = split(ll, " -> ")
-    x1, y1 = parse.(Int, split(x1y1, ","))
-    x2, y2 = parse.(Int, split(x2y2, ","))
-
-    if hvline(x1,y1,x2,y2)
-        x1, x2 = min(x1,x2), max(x1,x2)
-        y1, y2 = min(y1,y2), max(y1,y2)
-        zz[x1+1:x2+1, y1+1:y2+1] .+= 1
-    else
-        diag = diagonal(x1,y1,x2,y2)
-        for (x,y) in zip(collect(diag[1]), collect(diag[2]))
-            zz[x+1, y+1] += 1
+function all_lines(input)
+    zz = zeros(Int, 1000, 1000)
+    for line ∈ input
+        (x1,y1),(x2,y2) = fxy(line)
+        if hvline(x1,y1,x2,y2)
+            x1, x2 = minmax(x1,x2)
+            y1, y2 = minmax(y1,y2)
+            zz[x1+1:x2+1, y1+1:y2+1] .+= 1
+        else
+            diag = diagonal(x1,y1,x2,y2)
+            for (x,y) in zip(collect(diag[1]), collect(diag[2]))
+                zz[x+1, y+1] += 1
+            end
         end
     end
+    count(zz .> 1)
 end
-println(count(zz .> 1))
+
+@time part1 = "data/2021/input05.txt" |> readlines |> only_hv
+@time part2 = "data/2021/input05.txt" |> readlines |> all_lines
+
+println("Part 1 : $(part1)")
+println("Part 2 : $(part2)")
