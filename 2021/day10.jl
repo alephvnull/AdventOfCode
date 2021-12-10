a1d093478d4f5ac0638bff025acef533714c7ed1
genkuroo <genkurouy@gmail.com>
2021-12-10 14:35:34 +0100

    2021 / day 10

--- 2021/day10.jl
+++ 2021/day10.jl
@@ -1,75 +1,51 @@
 const input = split.(readlines("data/2021/input10.txt"), "")
 
-const symmetry = Dict("}"=>"{", ">"=>"<", ")" => "(", "]" => "[")
+const symmetry = Dict("}" => "{", ">" => "<", ")" => "(", "]" => "[")
 
-const scorec = Dict("}"=>1197, ">"=>25137, ")" => 3, "]" => 57,
-                    "{"=>3,    "<"=>4,     "(" => 1, "[" => 2)
+const scorec = Dict("}" => 1197, ">" => 25137, ")" => 3, "]" => 57,
+                    "{" => 3,    "<" => 4,     "(" => 1, "[" => 2)
 
 opening(cc) = cc == "{" || cc == "(" || cc == "[" || cc == "<"
 
-function corrupted()
+
+function corrupted(line)
     score = 0
-    for line ∈ input
-        stack = String[]
-        for cc ∈ line
-            if length(stack) != 0
-                if opening(cc)
-                    push!(stack,cc)
-                else 
-                    pop!(stack) != symmetry[cc] ? score += scorec[cc] : nothing       
-                end
-            else
+    stack = String[]
+    for cc ∈ line
+        if length(stack) != 0
+            if opening(cc)
                 push!(stack,cc)
+            else 
+                pop!(stack) != symmetry[cc] ? score += scorec[cc] : nothing       
             end
+        else
+            push!(stack,cc)
         end
     end
-    println(score)
+    score
 end
 
-function dd2()
-    score = 0
-    
-    ax = Array([])
-    for line ∈ input
-        corrupted = false
+stack_score(st) = foldl((α,β) -> 5*α+β, map(x -> scorec[x],reverse(st)), init = 0)
+
+function incomplete(input)
+    score = input .|> corrupted |> t -> map([x for (i,x) ∈ enumerate(input) if t[i] == 0]) do line
         stack = String[]
         for cc ∈ line
-            if length(stack) != 0
-                if opening(cc)
-                    push!(stack,cc)
-                else 
-                    pop!(stack) != symmetry[cc] ? (score += scorec[cc]; corrupted = true) : nothing       
-                end
-            else
-                push!(stack,cc)
-            end
-        end
-
-        if corrupted == false
-            push!(ax, line)
-        end
-    end
-    score2 = Array([])
-
-    for line in ax 
-        stack = Array([])
-        for cc in line
             if length(stack) != 0 
-                if opening(cc)
-                    push!(stack,cc)
-                elseif stack[end] == symmetry[cc]
-                    pop!(stack)
-                end    
+                opening(cc) ? push!(stack,cc) : pop!(stack)   
             else
                 push!(stack,cc)
             end
         end
-        push!(score2, foldl((a,b) -> 5*a+b, map(x -> scorec[x],reverse(stack)), init = 0) )
-    end
-    sort(score2)[Int(ceil(end/2))]
+        stack
+    end  .|> stack_score |> sort 
+    score[Int(ceil(end/2))] 
 end
 
-corrupted()
-@time dd2()
+@time part1 = input .|> corrupted |> sum
+@time part2 = input |> incomplete
+
+println("Part 1 : $(part1)")
+println("Part 2 : $(part2)")
 
 
