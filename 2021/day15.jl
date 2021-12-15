1f5d78bb0980be4095071ccfeca359a3cd00e09c
genkuroo <genkurouy@gmail.com>
2021-12-15 21:43:20 +0100

    2021 / day 15

---
+++ 2021/day15.jl
@@ -0,0 +1,35 @@
+using DataStructures
+
+const input = parse.(Int, reduce(hcat, collect.(readlines("data/2021/input15.txt"))))
+const adjset  = [(-1,0),(1,0), (0,-1),(0,1)]
+
+transform(x) = ([
+        x    x.+1 x.+2 x.+3 x.+4
+        x.+1 x.+2 x.+3 x.+4 x.+5
+        x.+2 x.+3 x.+4 x.+5 x.+6
+        x.+3 x.+4 x.+5 x.+6 x.+7
+        x.+4 x.+5 x.+6 x.+7 x.+8
+    ] .- 1) .% 9 .+ 1
+
+function dijkstra(input)
+    height, width = size(input)
+    adjacent(x,y) = [(x,y).+adj for adj ∈ adjset if x+adj[1] ∈ 1:height && y+adj[2] ∈ 1:width]
+
+    queue = PriorityQueue((1, 1) => 0)
+    visited = Set(((1, 1),))
+    while !isempty(queue)
+        (xx, yy), len = dequeue_pair!(queue)
+        push!(visited, (xx, yy))
+        (xx, yy) == (height, width) ? (return len) : nothing
+        for (nxx, nyy) in adjacent(xx,yy)
+            if (nxx, nyy) ∉ visited
+                newlen = len + input[nxx, nyy]
+                newlen < get(queue, (nxx, nyy), Inf) ? queue[(nxx, nyy)] = newlen : nothing
+            end
+        end
+    end
+end
+
+@time input |> dijkstra |> println
+@time input |> transform |> dijkstra |> println
+
