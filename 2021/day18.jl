1f6092e2f6048177e522cf7192ce3edc6bead834
genkuroo <genkurouy@gmail.com>
2021-12-18 15:10:06 +0100

    2021 / day 18

--- 2021/day18.jl
+++ 2021/day18.jl
@@ -1,48 +1,38 @@
-input = readlines("data/2021/input18.txt")
-
 ⨭(p,n) = typeof(p) <: Int ? p + n : [p[1] ⨭ n, p[2]] # add most left
 
 ⨮(p,n) = typeof(p) <: Int ? p + n : [p[1], p[2] ⨮ n ] # add most right
 
-~(p) = typeof(p) <: Int ? p : 3*~(p[1]) + 2*~(p[2]) # magnitude
+~(p) = typeof(p) <: Int ? p : 3*~p[1] + 2*~p[2] # magnitude
 
-function ⩝(p,d) #explode
+function ⋄(p,d) #explode
     typeof(p) <: Int ?  (return p, 0, 0, false) : nothing
-
-    if d < 4 
-        r, al, ar, m = p[1] ⩝ (d+1)
-        m  ? (return [r, p[2] ⨭ ar], al, 0, m) : nothing
-
-        r, al, ar, m = p[2] ⩝ (d+1)
-        m ? (return [p[1] ⨮ al, r], 0, ar, m) : nothing
-
-        return p, 0, 0, false
-    else  
+    if d == 4 
         0, p[1], p[2], true
+    else  
+        zr, L, R, mod = p[1] ⋄ (d+1)
+        mod  ? (return [zr, p[2] ⨭ R], L, 0, mod) : nothing
+
+        zr, L, R, mod = p[2] ⋄ (d+1)
+        mod ? (return [p[1] ⨮ L, zr], 0, R, mod) : nothing
+
+        p, 0, 0, false
     end
 end
 
-function !(p) #split
-    if typeof(p) <: Int
-        if p < 10 
-            return p, false
-        else
-            return [Int(floor(p//2)), Int(ceil(p//2))], true
-        end
+∛(p) = p < 10 ? (p, false) : ([Int(floor(p//2)), Int(ceil(p//2))], true)
 
-    else
-        L, R = p
-        a, ra = !L
-        if ra
-            return [a,R], true
-        end
-        b, rb = !R
-        return [a,b], rb
-    end
+function ∜(p)
+    L, R = p
+    L, FL = !L
+    FL ? (return [L,R], true) : nothing
+    R, FR = !R
+    [L,R], FR
 end
+
+!(p) = typeof(p) <: Int ? ∛p : ∜p #split
 
 function ¬(p) #reduce
-    px, _, _, mod = p ⩝ 0
+    px, _, _, mod = p ⋄ 0
     mod ? (return ¬px) : nothing
     px, mod = !px
     mod ? ¬px : px
@@ -50,9 +40,7 @@ end
 
 ⊕(x,y) = ¬[x,y] # addition
 
-str2num(x) = eval(Meta.parse(x))
-
-~reduce(⊕,str2num.(input)) |> println
-inp = str2num.(input)
-maximum([~(x ⊕ y)  for x in inp for y in inp if y ≠ x ])
+const inp = readlines("data/2021/input18.txt") .|> Meta.parse .|> eval
+@time "Part:1 $(~reduce(⊕, copy(inp)))"  |> println
+@time "Part 2: $(maximum([~(x ⊕ y)  for x ∈ inp for y ∈ inp if y ≠ x ]))"  |> println
 
