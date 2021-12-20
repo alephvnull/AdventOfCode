a72defe428fd4fd26ea1ea333d10bccf85875eca
genkuroo <genkurouy@gmail.com>
2021-12-20 16:18:37 +0100

    2021 / day 20

--- 2021/day20.jl
+++ 2021/day20.jl
@@ -1,46 +1,37 @@
 input = readlines("data/2021/input20.txt")
 
-Δ = Dict([i-1 => (x == "#" ? "1" : "0") for (i,x) in enumerate(split(input[1], ""))])
+const Δ = Dict([i-1 => (x == "#" ? "1" : "0") for (i,x) ∈ enumerate(split(input[1], ""))])
+const α = Δ[0] == "1" && Δ[511] == "0"
 
 bit2int(x) = Δ[parse(Int, reduce(*, x), base = 2)]
 
-α = Δ[0] == "1" && Δ[511] == "0"
-
-#if not in a dict get default that is alternating based on alpha
-
-image = Dict((i,j) => (char == '#' ? "1" : "0")
-    for (i,row) in enumerate(input[3:end]) 
-    for (j,char) in enumerate(row)
+const image = Dict((i,j) => (char == '#' ? "1" : "0")
+    for (i,row) ∈ enumerate(input[3:end]) 
+    for (j,char) ∈ enumerate(row)
 )
 
-adjset = [(-1,-1), (-1,0), (-1,1), (0,-1), (0,0),(0,1), (1,-1), (1,0),(1,1)]
+const adjset = [(-1,-1), (-1,0), (-1,1), 
+                (0, -1), (0, 0), (0, 1), 
+                (1, -1), (1, 0), (1, 1)]
 
-adjacent(x,y) = [(x,y).+adj for adj ∈ adjset]
+adjacent(x,y) = [(x,y) .+ adj for adj ∈ adjset]
 
 mask(cp, rw, cl, def) = map(x -> haskey(cp, x) ? cp[x] : def, adjacent(rw,cl)) |> bit2int 
     
-function process()
-    cp = image
-     
-    def = "0"
-
-    step = 50
-    for i in 1:step
+function process(step)
+    cp, def = image, "0"
+    mini, maxi, minj, maxj  = 1, 100, 1, 100
+    for _ ∈ 1:step
         nimage = Dict()
-        maxi = maximum([i for ((i,j), k) in cp])
-        mini = minimum([i for ((i,j), k) in cp])
-        maxj = maximum([j for ((i,j), k) in cp])
-        minj = minimum([j for ((i,j), k) in cp])
-
-        for rw in mini-2:maxi+3, cl in minj-2:maxj+3
+        mini -= 2; maxi += 2; minj -= 2; maxj +=2
+        for rw ∈ mini:maxi, cl ∈ minj:maxj
             push!(nimage, (rw,cl) => mask(cp, rw, cl, def))
         end
-
         α ? def = (def == "0" ? "1" : "0") : nothing
         cp = nimage
     end
-
     sum(parse.(Int,values(cp)))
-   
 end
-process()
+
+@time "Part 1: $(process(2))"  |> println
+@time "Part 2: $(process(50))" |> println
