ebf1096eb0d8c72b48ba8f74246c5ed40e069cf1
genkuroo <genkurouy@gmail.com>
2021-12-23 05:26:45 +0100

    2021 / day 1

--- 2021/day01.jl
+++ 2021/day01.jl
@@ -1,23 +1,8 @@
-function parse_file(file_path) 
-    ln = Int64[]
-    open(file_path) do file
-        for line in eachline(file)
-            push!(ln, parse(Int, line))
-        end
-    end
-    return ln
-end
+const input = "data/2021/input01.txt" |> readlines .|> x -> parse(Int, x)
 
-function increases(ln)
-    return [ 1 for i in 2:length(ln) if ln[i] > ln[i-1]] |> length
-end
+increases(ln) = [1 for i ∈ 2:length(ln) if ln[i] > ln[i-1]] |> length
 
-function threesumincreases(ln)
-    return [ 1 for i in 4:length(ln) if ln[i] > ln[i-3]] |> length
-end
+threesumincreases(ln) = [1 for i ∈ 4:length(ln) if ln[i] > ln[i-3]] |> length
 
-@time part1 = "data/2021/input01.txt" |> parse_file |> increases
-@time part2 = "data/2021/input01.txt" |> parse_file |> threesumincreases 
-
-println("Part 1 : $(part1)")
-println("Part 2 : $(part2)")
+@time input |> increases |> x-> "p1: $(x)" |> println
+@time input |> threesumincreases |> x-> "p2: $(x)" |> println
