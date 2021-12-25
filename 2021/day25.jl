c01c81914a30639e5ec10b2696bcda8ab6087140
genkuroo <genkurouy@gmail.com>
2021-12-25 12:08:33 +0100

    2021 / day 25

--- 2021/day25.jl
+++ 2021/day25.jl
@@ -1,45 +1,36 @@
-input = split.(readlines("data/2021/input25.txt"), "")
+const input = split.(readlines("data/2021/input25.txt"), "")
+lnx, lny = length(input), length(input[1])
+
+mp = Dict()
+for (i,line) ∈ enumerate(input)
+    for (j, y) ∈ enumerate(line)
+        if y == ">" || y == "v" push!(mp, (i,j) => y) end
+    end
+end
 
 function simulate()
-
-    
-    cp = Dict()
-
-    for (i,x) in enumerate(input)
-        for (j, y) in enumerate(x)
-            if y == ">" || y == "v"   push!(cp, (i,j) => y) end
-        end
-    end
-    lnx = length(input)
-    lny = length(input[1])
-    step = 0
+    cp, step = deepcopy(mp), 0
     while true
         step += 1
-        new = deepcopy(cp)
-        change = false
-        for ((i,j),v) in cp
+        new, change = copy(cp), false
+        for ((i,j),v) ∈ cp
             if v == ">" && !haskey(cp,  (i, j%lny+1))
                 delete!(new, (i,j))
                 push!(new, (i, j%lny+1) => ">")
                 change = true
             end
         end
-        cp = deepcopy(new)
-        new = deepcopy(cp)
-        for ((i,j),v) in cp
+        cp = copy(new)
+        for ((i,j),v) ∈ cp
             if v == "v" && !haskey(cp, (i%lnx+1,j))
                 delete!(new, (i,j))
                 push!(new, (i%lnx+1, j) => "v")
                 change = true
             end
         end
-        if !change break end
-        cp = deepcopy(new)
-        
+        !change ? break : cp = copy(new)
     end
     step
-
 end
 
-simulate()
-
+simulate() |> x -> "p1: $(x)" |> println
